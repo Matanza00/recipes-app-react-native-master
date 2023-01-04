72ac5d50637f95de5dff4fa53dba3e5d1369ed8b
Usman Nadeem <88729594+Matanza00@users.noreply.github.com>
2023-01-04 10:23:37 +0500

    Initial commit

---
+++ src/data/dataArrays.js
@@ -0,0 +1,74 @@
+export const categories = [
+  {
+    id: 1,
+    name: 'Profile',
+    photo_url: 'https://i.pinimg.com/originals/ea/dc/8e/eadc8ee72556cd654032295d2b77f8fe.jpg?imwidth=1400'
+  },
+  {
+    id: 2,
+    name : 'Car',
+    photo_url: 'https://static.pakwheels.com/2020/12/cars-in-2020-blog-featured.jpg?imwidth=1400'
+  },
+  {
+    id: 3,
+    name: 'Camera',
+    photo_url:
+    'https://editor.analyticsvidhya.com/uploads/88060Learn-Facial-Recognition-scaled.jpg?imwidth=1400'
+  },
+  {
+    id: 4,
+    name: 'Analytics',
+    photo_url:
+    'https://www.analyticsinsight.net/wp-content/uploads/2021/01/Analytics-1024x622.jpeg?crop=0.715xw:0.534xh;0.0945xw,0.451xh&resize=768:*'
+  },
+
+];
+
+export const recipes = [
+  {
+    recipeId: 122,
+    categoryId: 3,
+    title: 'Camera',
+    photo_url: 'https://editor.analyticsvidhya.com/uploads/88060Learn-Facial-Recognition-scaled.jpg?imwidth=1400',
+    photosArray: [
+      'https://miro.medium.com/max/660/1*enzZrRQ_EwtfJJKOffrcFg.png',
+      "https://www.antoniofurioso.com/wp-content/uploads/2022/01/man-gea55c828c_1280-1-1024x679.jpg",
+    ],
+    detail: 'Face Detection',
+  },
+  {
+    recipeId: 6,
+    categoryId: 2,
+    title: 'Car',
+    photo_url: 'https://static.pakwheels.com/2020/12/cars-in-2020-blog-featured.jpg?imwidth=1400',
+    photosArray: [
+      'https://i.brecorder.com/primary/2021/05/60b55d560d523.jpg',
+      "https://www.shutterstock.com/image-photo/cars-sale-stock-row-car-260nw-636632101.jpg",
+    ],
+    detail: 'Vehicles',
+  },
+  {
+    recipeId: 3,
+    categoryId: 4,
+    title: 'Analytics',
+    photo_url: 'https://miro.medium.com/max/702/1*Ra02AqsQlC0KV229EvM98g.png?imwidth=1400',
+    photosArray: [
+      'https://www.simplilearn.com/ice9/free_resources_article_thumb/Types_of_Business_Analytics.jpg',
+      "https://miro.medium.com/max/702/1*Ra02AqsQlC0KV229EvM98g.png",
+      'https://www.travelpayouts.com/blog/wp-content/uploads/2020/07/how-to-use-google-analytics-to-improve-it-projects.png',
+    ],
+    detail: 'OverAll Reports',
+  },
+  {
+    recipeId: 4,
+    categoryId: 1,
+    title: 'Profile',
+    photo_url: 'https://i.pinimg.com/originals/d9/56/9b/d9569bbed4393e2ceb1af7ba64fdf86a.jpg?imwidth=1400',
+    photosArray: [
+      'https://i.pinimg.com/originals/d9/56/9b/d9569bbed4393e2ceb1af7ba64fdf86a.jpg',
+    ],
+    detail: 'Settings Profile',
+  }
+  
+];
+
